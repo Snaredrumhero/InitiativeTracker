45e4a8729d0d9b934af8984fd78174de1c3c804c
Drew Mullett <dmullett02@gmail.com>
2023-10-22 01:33:04 -0400

    Added test json file generation and can import json files

--- InitiativeTracker/MainWindow.xaml.cs
+++ InitiativeTracker/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,17 +18,19 @@ using System.Windows.Shapes;
 
 namespace InitiativeTracker
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Player> Players = new List<Player>();
+
         public MainWindow()
         {
             InitializeComponent();
             // Before I see the window
             // Additional code is below me!
-            List<Player> Players = new List<Player>();
         }
 
         private void NextTurn_Button_OnClick(object sender, RoutedEventArgs e)
@@ -41,19 +46,52 @@ namespace InitiativeTracker
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            
+            //Player testPlayer = new ();
+            //Player testPlayer2 = new (5, 5, 5, "Candace", "Tech Specialist");
+            //Player testPlayer3 = new (10, 10, 10, "Kang", "Monk Goliath");
+            //Player testPlayer4 = new (15, 15, 15, "Drew Mullett", "Fucking crazy");
+            //List<Player> testPlayers = new() { testPlayer, testPlayer2, testPlayer3, testPlayer4 };
+
+            //string jsonString = JsonConvert.SerializeObject(testPlayers, Formatting.Indented);
+            //File.WriteAllText("test.json", jsonString);
+
             //When I see the window (after it appears, this runs)
         }
 
         private void LoadButton_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            // Open file explorer window
+            OpenFileDialog fileDialog = new()
+            {
+                Filter = "Json Files (*.json)|*.json|All Files (*.*)|*.*",
+                Title = "Select json file"
+            };
+            // Check if a file was selected
+            if (fileDialog.ShowDialog() is not true)
+            {
+                return;
+            }
+
+            string jsonString = File.ReadAllText(fileDialog.FileName);
+            List<Player> loadedPlayers = JsonConvert.DeserializeObject<List<Player>>(jsonString) ?? throw new Exception("Invalid json file");
+            // TODO Error handling
+            Players.AddRange(loadedPlayers);
+
+            RefreshGrid();
+            //TODO Display new players
         }
 
         private void RefreshButton_OnClick(object sender, RoutedEventArgs e)
         {
             throw new NotImplementedException();
         }
-    }
+
+        private void RefreshGrid()
+        {
+            throw new NotImplementedException();
+            
+        }
+    }   
+        
 }
     
--- InitiativeTracker/Player.cs
+++ InitiativeTracker/Player.cs
@@ -6,35 +6,24 @@ using System.Threading.Tasks;
 
 namespace InitiativeTracker
 {
-    internal class Player
+    public class Player
     {
-        public Player(int constructInitiative, int constructMaxHealth, int constructArmorClass, 
-            string constructName, string constructNotes)
+        public Player(int constructInitiative = 0, int constructMaxHealth = 0, int constructArmorClass = 0, 
+            string constructName = "", string constructNotes = "")
         {
-            initiative = constructInitiative;
-            current_health = constructMaxHealth;
-            max_health = constructMaxHealth;
-            armor_class = constructArmorClass;
-            name = constructName;
-            notes = constructNotes;
+            Initiative = constructInitiative;
+            CurrentHealth = constructMaxHealth;
+            MaxHealth = constructMaxHealth;
+            ArmorClass = constructArmorClass;
+            Name = constructName;
+            Notes = constructNotes;
         }
-
-        public Player()
-        {
-            initiative = -1;
-            current_health = 0;
-            max_health = 0;
-            armor_class = 0;
-            name = "Enemy 1";
-            notes = "";
-        }
-
-
-        private int initiative;
-        private int current_health;
-        private int max_health;
-        private int armor_class;
-        private string name;
-        private string notes;
+        
+        public int Initiative;
+        public int CurrentHealth;
+        public int MaxHealth;
+        public int ArmorClass;
+        public string Name;
+        public string Notes;
     }
 }
